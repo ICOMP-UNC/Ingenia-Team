33c3ba3a6ae12eb6e342d1a5f89ca78fc1eb4866
JuanPSanchez <juan.sanchez.busso@mi.unc.edu.ar>
2024-11-10 00:40:25 -0300

    Update gpio.c
    Se implemento la configuración inicial de los puertos GPIO y EINT, junto con la declaración de la función toggle_leds

--- gpio.c
+++ gpio.c
@@ -1,1 +1,84 @@
+/** 
+ * @file gpio.c
+ * @brief Configure the ports that are going to be use in this project.
+ * There's two GPIO pins, P0.0 and P0.1, on each one there's an LED, 
+ * and two External Interrupt pins P2.10 and P2.11, to interact with the ADC module.
+*/
 
+#include "LPC17xx.h"
+#include "lpc17xx_gpio.h"   /* GPIO handling */
+#include "lpc17xx_pinsel.h" /* Pin function selection */
+
+/* Pin Definitions */
+#define CHANNEL_PIN   ((uint32_t)(1 << 10)) /* P2.10 connected to button */
+#define SCALE_PIN ((uint32_t)(1 << 11))  /* P2.11 connected to button */
+#define RED_LED_PIN ((uint32_t)(1 << 0))  /* P0.0 connected to Red LED */
+#define GREEN_LED_PIN ((uint32_t)(1 << 1))  /* P0.1 connected to Green LED */
+
+
+/* GPIO Direction Definitions */
+#define INPUT  0
+#define OUTPUT 1
+
+/* Boolean Values */
+#define TRUE  1
+#define FALSE 0
+
+/**
+ * @brief Initialize the GPIO peripheral
+ *
+ */
+void configure_ports(void)
+{
+    PINSEL_CFG_Type pin_cfg; /* Create a variable to store the configuration of the pin */
+
+    /* Configure the pin P0.0 as GPIO */
+    led_pin_cfg.Portnum = PINSEL_PORT_0;           /* The port number is 0 */
+    led_pin_cfg.Pinnum = PINSEL_PIN_0;            /* The pin number is 0 */
+    led_pin_cfg.Funcnum = PINSEL_FUNC_0;           /* The function number is 0 */
+    led_pin_cfg.Pinmode = PINSEL_PINMODE_PULLUP;   /* The pin mode is pull-up */
+    led_pin_cfg.OpenDrain = PINSEL_PINMODE_NORMAL; /* The pin is in the normal mode */
+    PINSEL_ConfigPin(&pin_cfg);
+
+    /* Configure the pin P0.1 as GPIO */
+    led_pin_cfg.Pinnum = PINSEL_PIN_1;            /* The pin number is 0 */
+    PINSEL_ConfigPin(&pin_cfg);
+     
+    /* Configure the pin P2.10 as EINT0 */
+    led_pin_cfg.Portnum = PINSEL_PORT_2;           /* The port number is 2 */
+    led_pin_cfg.Pinnum = PINSEL_PIN_10;            /* The pin number is 10 */
+    led_pin_cfg.Funcnum = PINSEL_FUNC_1;           /* The function number is 1 */
+    led_pin_cfg.Pinmode = PINSEL_PINMODE_PULLUP;   /* The pin mode is pull-up */
+    led_pin_cfg.OpenDrain = PINSEL_PINMODE_NORMAL; /* The pin is in the normal mode */
+    PINSEL_ConfigPin(&pin_cfg);
+
+    /* Configure the pin P2.10 as EINT1 */
+    led_pin_cfg.Portnum = PINSEL_PORT_2;           /* The port number is 2 */
+    led_pin_cfg.Pinnum = PINSEL_PIN_11;            /* The pin number is 11 */
+    led_pin_cfg.Funcnum = PINSEL_FUNC_1;           /* The function number is 1 */
+    led_pin_cfg.Pinmode = PINSEL_PINMODE_PULLUP;   /* The pin mode is pull-up */
+    led_pin_cfg.OpenDrain = PINSEL_PINMODE_NORMAL; /* The pin is in the normal mode */
+    PINSEL_ConfigPin(&pin_cfg);
+
+    /* Set the pins as P.0 and P0.1 as outputs */
+    GPIO_SetDir(PINSEL_PORT_0, RED_LED_PIN | GREEN_LED_PIN, OUTPUT);  
+}
+
+
+/**
+ * @brief function to toggle between one or the other LED
+ * as a way to know what ADC channel or oscilloscope channel is active.
+ */
+void toggle_leds(void){
+    if (GPIO_ReadValue(PINSEL_PORT_0) & RED_LED_PIN)
+    {
+        GPIO_ClearValue(PINSEL_PORT_0, RED_LED_PIN); /* Turn off Red LED */
+        GPIO_SetValue(PINSEL_PORT_0, GREEN_LED_PIN); /* Turn on Green LED */
+    }
+    else
+    {
+        GPIO_ClearValue(PINSEL_PORT_0, GREEN_LED_PIN); /* Turn off Green LED */
+        GPIO_SetValue(PINSEL_PORT_0, RED_LED_PIN); /* Turn on Red LED */
+    }
+
+}
