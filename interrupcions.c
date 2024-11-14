bb109fe4de179cc380fb68a229d83afe7c37d1fa
JuanPSanchez <juan.sanchez.busso@mi.unc.edu.ar>
2024-11-14 00:05:53 -0300

    Update interrumpions.c
    Implemented EINT1 to switch between ADC channels

--- interrupcions.c
+++ interrupcions.c
@@ -6,20 +6,12 @@
 
 
 #include "LPC17xx.h"
-<<<<<<< Updated upstream
-#include "lpc17xx_exti.h"    /* External interrupt handling */
-
-/* Pin Definitions */
-#define FREQ_PIN ((uint32_t)(1 << 10))  /* P2.10 connected to switch */
-#define SCALE_PIN ((uint32_t)(1 << 10))  /* P2.11 connected to switch */
-=======
 #include "lpc17xx_exti.h"   
 #include "lpc17xx_adc.h"
 #include "gpio.c"
 
 /* Pin Definitions */
 #define FREQ_PIN ((uint32_t)(1 << 10))  /* P2.10 connected to switch */
->>>>>>> Stashed changes
 
 /* GPIO Direction Definitions */
 #define INPUT  0
@@ -29,12 +21,9 @@
 #define TRUE  1
 #define FALSE 0
 
-<<<<<<< Updated upstream
-=======
 /* Variables */ 
 volatile uint8_t adc_current_channel = 0;
 
->>>>>>> Stashed changes
 /* Function prototypes */
 
 /**
@@ -49,17 +38,10 @@ void configure_external_interrupt(void);
 void EINT0_IRQHandler(void);
 
 /**
-<<<<<<< Updated upstream
- * @brief Overwrite the interrupt handle routine for GPIO (EINT1)
- * Changes the scale 
- */
-void EINT1_IRQHandler(void);
-=======
  * @brief Overwrite the interrupt handle routine for GPIO (EINT3)
  *  
  */
 void EINT3_IRQHandler(void);
->>>>>>> Stashed changes
 
 
 
@@ -73,27 +55,11 @@ void configure_external_interrupt(void)
 	exti_cfg.EXTI_polarity = EXTI_POLARITY_LOW_ACTIVE_OR_FALLING_EDGE;
 	EXTI_Config(&exti_cfg);
 
-<<<<<<< Updated upstream
-    /* Configure EINT1 as falling edge */
-    exti_cfg.EXTI_Line = EXTI_EINT1;
-    EXTI_Config(&exti_cfg);
-=======
->>>>>>> Stashed changes
 }	
 
 // Overwrite the interrupt handle routine for EINT0
 void EINT0_IRQHandler(void)
 {
-<<<<<<< Updated upstream
-    EXTI_ClearEXTIFlag(EXTI_EINT0); /* Clear the interrupt flag */
-    
-}
-
-// Overwrite the interrupt handle routine for EINT1
-void EINT0_IRQHandler(void)
-{
-    EXTI_ClearEXTIFlag(EXTI_EINT1); /* Clear the interrupt flag */
-=======
     // Alterna el canal entre 0 y 1
     adc_current_channel = (adc_current_channel == 0) ? 1 : 0;
     
@@ -110,7 +76,6 @@ void EINT0_IRQHandler(void)
 void EINT3_IRQHandler(void)
 {
     EXTI_ClearEXTIFlag(EXTI_EINT3); /* Clear the interrupt flag */
->>>>>>> Stashed changes
     
 }
 
